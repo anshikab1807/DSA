934d7a29da9c2789c3fb0a9c55c88ddd6c71737d
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2025-08-12 23:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (84.62%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
-        
-        if (p != null && q != null && p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        }
-        
-        return false;        
-    }
+      if(p== null && q== null)
+      {
+        return true;
+      }
+      if( p!= null && q!=null && p.val == q.val ){
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+      }
+      return false;
+}
 }
