6bbb5a2a01ab22fbeedf0726ebd5b31bd415cc0c
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2026-01-12 20:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (35.43%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,7 +2,8 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean zero = false;
         for (int row = 0; row < matrix.length; row++) {
-            if (matrix[row][0] == 0) zero= true;
+            if (matrix[row][0] == 0)
+                zero = true;
             for (int col = 1; col < matrix[0].length; col++) {
                 if (matrix[row][col] == 0) {
                     matrix[row][0] = 0;
@@ -10,7 +11,6 @@ class Solution {
                 }
             }
         }
-
         for (int row = matrix.length - 1; row >= 0; row--) {
             for (int col = matrix[0].length - 1; col >= 1; col--) {
                 if (matrix[row][0] == 0 || matrix[0][col] == 0) {
