c576edc65ac16f677c9700ab7f70ce007bef03a3
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2026-02-12 10:58:32 +0530

    Time: 185 ms (10.05%) | Memory: 48.5 MB (5.36%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,16 +13,35 @@
  *     }
  * }
  */
+// class Solution {
+//     int d=0;
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         height(root);
+//         return d;
+//     } public int height(TreeNode node){
+//         if(node==null) return 0;
+//         int left=height(node.left);
+//         int right=height(node.right);
+//         d=Math.max(d,left+right);
+//         return 1+Math.max(left,right);
+//     }
+// }
+
 class Solution {
-    int d=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        height(root);
-        return d;
-    } public int height(TreeNode node){
-        if(node==null) return 0;
-        int left=height(node.left);
-        int right=height(node.right);
-        d=Math.max(d,left+right);
-        return 1+Math.max(left,right);
+        if (root == null) {
+            return 0;
+        }
+        int lh= height(root.left);
+        int rh= height(root.right);
+        int d1=diameterOfBinaryTree(root.left);
+        int d2=diameterOfBinaryTree(root.right);
+        return Math.max((lh+rh),Math.max(d1,d2));
+    }
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+    return Math.max(height(node.left),height(node.right))+1;
     }
 }
