8ee6b0fcbb07c9c0b38b9e56b0bdce3b48a91771
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2026-07-06 23:29:17 +0530

    Time: 1 ms (98.62%) | Memory: 47.4 MB (59.50%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int t=0;
+        for (int num : nums) t+=num;
+        int l=0; 
+        for(int i=0;i<nums.length;i++) {
+            int r=t-l-nums[i];
+            if(l==r) return i; 
+            l+=nums[i]; 
+        }
+        return -1; 
+    }
+}
